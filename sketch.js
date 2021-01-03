88aadb6b17a380f02a4b601aa913939cf6c6261c
umakiran-whj <umaleelavathi@gmail.com>
2021-01-03 18:23:19 +0530

    add platform

--- sketch.js
+++ sketch.js
@@ -6,6 +6,8 @@ var engine, world;
 var box1, pig1;
 var backgroundImg;
 
+var platform;
+
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -17,6 +19,7 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
+platform=new Ground(300,350,300,600);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -48,6 +51,8 @@ function draw(){
     ground.display();
     pig1.display();
     log1.display();
+
+platform.display();
 
     box3.display();
     box4.display();
